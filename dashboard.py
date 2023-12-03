06d91abc1719df520701168772c07ea778812e08
wahyu ikbal maulana <wahyuikbal777@gmail.com>
2023-12-03 22:31:40 +0700

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,6 @@ import seaborn as sns
 import plotly.express as px
 import numpy as np
 import requests
-import openpyxl
 from pandas import json_normalize
 from streamlit_extras.add_vertical_space import add_vertical_space
 from streamlit_lottie import st_lottie
@@ -32,18 +31,15 @@ df = df.iloc[:-5]
 data = df.pivot(index='Country Name', columns='Series Name', values='2019 [YR2019]')
 data.reset_index(inplace=True)
 
-column_names = ['Country', 'CO2 Emission', 'Health Expenditure', 'GDP', 'Immunization DPT', 'Immunization HepB3', 'Immunization Measles', 'Life Expectancy', 'Infant Death', 'Maternal Death', 'Primary Education']
+column_names = ['country', 'CO2 Emission', 'Health Expenditure', 'GDP', 'Immunization DPT', 'Immunization HepB3', 'Immunization Measles', 'Life Expectancy', 'Infant Death', 'Maternal Death', 'Primary Education']
 data.columns = column_names
 
-data["Country"] = data["Country"].str.replace(',', '')
+data["country"] = data["country"].str.replace(',', '')
 data = data.replace(["..", "..."], np.nan)
 data.dropna(inplace=True)
 
 population = pd.read_excel("population.xlsx")
-data = pd.merge(data, population, on='Country')
-
-cols = data.select_dtypes(exclude=['object'])
-
+data = pd.merge(data, population, on='country')
 
 row0_spacer1, row0_1, row0_spacer2, row0_2, row0_spacer3 = st.columns(
     (0.1, 2, 0.2, 1, 0.1)
@@ -88,8 +84,8 @@ with row2_1:
             - $\\epsilon$ is the random error unexplained by the model.
         ''')
 
-    st.info(
-        """Pastikan untuk menggunakan light theme dengan mengklik tombol titik tiga di pojok kanan atas, lalu klik setting dan pilih Light theme
+    st.warning(
+        """🙏 Pastikan untuk menggunakan light theme dengan mengklik tombol titik tiga di pojok kanan atas, lalu klik setting dan pilih Light theme
         """
     )
     ok = st.expander("Dataset here")
@@ -113,29 +109,82 @@ st.write("")
 row3_space1, row3_1, row3_space2, row3_2, row3_space3 = st.columns(
     (0.1, 1, 0.1, 1, 0.1)
 )
+st.markdown("")
 
 #Plot1
 with row3_1:
-    st.subheader("Books Read")
+    st.subheader("Scatter Plot: Health Expenditure vs Life Expectancy")
     scatter_fig = px.scatter(data, x="Health Expenditure", y="Life Expectancy",
-                             size="population", hover_name="Country", log_x=True, size_max=50,
-                             title='Scatter Plot: Health Expenditure vs Life Expectancy')
+                             size="population", hover_name="country", log_x=True, size_max=50)
 
     st.plotly_chart(scatter_fig, theme="streamlit", use_container_width=True)
     st.markdown(
-        "It looks like you've read a grand total of  books with authors,** with being your most read author! That's awesome. Here's what your reading habits look like since you've started using Goodreads."
+        "The regression between life expectancy and health expenditure suggests that there is a relationship between a population's average lifespan and the amount of resources invested in healthcare. Generally, higher health expenditures are associated with longer life expectancies, indicating that well-funded healthcare systems contribute to improved overall health and longevity in a population.."
         )
 
 with row3_2:
-    st.subheader("Books Read")
-    scatter_fig2 = px.scatter(data, x="Immunization DPT", y="Life Expectancy",
-                             size="population", hover_name="Country", log_x=True, size_max=50,
-                             title='Scatter Plot: Health Expenditure vs Life Expectancy')
+    st.subheader("Scatter Plot: Gross Domestic Product vs Life Expectancy")
+    scatter_fig2 = px.scatter(data, x="Immunization HepB3", y="Life Expectancy",
+                             size="population", hover_name="country", log_x=True, size_max=50)
 
     st.plotly_chart(scatter_fig2, theme="streamlit", use_container_width=True)
     st.markdown(
-        "It looks like you've read a grand total of  books with authors,** with being your most read author! That's awesome. Here's what your reading habits look like since you've started using Goodreads."
+        "The HepB3 schedule typically involves administering three doses of the Hepatitis B vaccine to provide immunity against the Hepatitis B virus This immunization is directly proportional to the high life expectancy. So each country must ensure equitable immunization, especially HepB3 immunization. "
         )
+    
+st.markdown("")
+st.markdown("")
+st.markdown("")
+st.markdown("")
 
+st.info(
+    "### Kesimpulan \n"
+    "aku wahyu"
+    
+)
+
+import statsmodels.api as sm 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+y = data["Life Expectancy"]
+x = data.drop(["country"], axis=1)
+x = x.drop(["Life Expectancy"], axis = 1)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3, random_state= 24)
+lr = LinearRegression()
+lr.fit(x_train, y_train)
+
+X_stats = sm.add_constant(x_train) 
+est = sm.OLS(y_train, X_stats).fit() 
+
+ok = st.expander("Statistical Analysis")
+with ok:
+    st.text(est.summary())
+ok1= st.expander("See explanation")
+with ok1:
+    st.markdown(r'''
+    First Section: Statistics of the Overall Linear Model. In a linear regression model of the form $$y = \beta_0 + \beta_1X + \epsilon$$ fitting $$N$$ data points with $$p$$ regressors and one regressand, several key statistics are computed. Residual Sum of Squares (RSS). The items on the first section of the summary are:
+    - No. Observations: The number of data points $$N$$
+    - Df model: Number of parameters in the model $$p$$
+    - Df residuals: Degree of freedom of the residuals, equals to $$N - p -1$$
+    - R-squared: $$R^2 = 1 - \frac{TSS}{RSS} = 1 - \frac{\sum_{i} (y_i - \bar{y})^2}{\sum_{i} (y_i - \hat{y}_i)^2}$$ as the coefficient of determination
+    - Adjusted R-squared:$${R}^2 = 1 - \frac{(1 - R^2)(n - 1)}{n - p - 1} = 1 - \frac{TSS/df_t}{RSS/df_e} = 1 - \frac{(1 - R^2)(n - 1)}{n - p - 1}$$
+    - F-statistic and Prob (F-statsitic): The null hypothesis that all the coefficients of regressors are zero, hence a high p-value means the model is more significant
+    - AIC: Akaike Information Criterion, $$ -2\log(\hat{L}) + 2k $$ with $$k = 2$$. It depends on the log-likelihood $$ L $$ and estimates the relative distance between the unknown true likelihood and the fitted likelihood. The lower the AIC, the closer to the truth.
+    - BIC: Bayesian Information Criterion, $$ -2\log(\hat{L}) + 2k $$ with $$k = log(N)$$. Based on a Bayesian set up and meansures the posterior probability of a model being true. The lower the BIC means the closer to the truth
+    ''')
+    st.markdown(r'''
+    The second section: Coefficients determined by the regression.
+    - Coef: Coefficient determined by OLS regression, it is solved analytically with $$ \beta = (X^T X)^{-1} X^T y $$
+    - Std Err: Estimate of the standard deviation of the coefficient, $$\hat{\sigma}_{j}^2 = \hat{\sigma}^2 \left[Q_{xx}^{-1}\right]_{jj}$$
+    where $\hat{\sigma}^2 = \frac{1}{N} \varepsilon^T \varepsilon$ and $Q_{xx} = X^T X$.
+    - t: Coef divided by Std Err, i.e., the t statistic, with the null hypothesis that this particular coefficient is zero. It is used as a measurement of whether theh coefficient is significant. A coefficient is significant if its magnitude is large with small standard error
+    - the p-value of the t test, i.e., the probability that the variable has no effect on the dependent variable as the null hypothesis is true
+    '''
+    )
+
+result = st.button( "Gallery Viz")
+if result:
+    st.write(": smile:")
 # Download link for notebook
-st.markdown("[Download Notebook](https://github.com/maulairfani/product-review-topic-modelling-LDA-curie/archive/refs/heads/main.zip)")
+#st.markdown("[Download Notebook](https://github.com/maulairfani/product-review-topic-modelling-LDA-curie/archive/refs/heads/main.zip)")
